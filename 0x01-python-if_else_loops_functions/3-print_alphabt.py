792d917f1cd3d6178e6e6300af5e897377c23904
olaniyo <opriolani>
2023-01-06 06:30:10 -0800

    task 3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) == 'q' or chr(i) == 'e':
+    if i == 101 or i == 113:
         continue
-    print(chr(i).format(i), end='')
+    else:
+        print("{}".format(chr(i)), end="")
