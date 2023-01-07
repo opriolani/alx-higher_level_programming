a9c694cbd47a02b0dd771274695e82b0bc53bbb3
olaniyo <opriolani>
2023-01-07 03:45:52 -0800

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-    print("{:d}".format(add))
+varLen = len(argv) - 1
+result = 0
+for i in range(1, varLen + 1):
+    result += int(argv[i])
+    print(result)
