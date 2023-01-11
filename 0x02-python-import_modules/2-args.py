52fb6700039e6e06f9fd70eac51111a292a85530
olaniyo <opriolani>
2023-01-11 08:20:08 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
 
-    lenOfArg = len(argv)
-    if lenOfArg == 1:
-        print("0 arguments.")
-    elif lenOfArg == 2:
-        print("{} argument:".format(lenOfArg - 1))
-    else:
-        print("{} arguments:".format(lenOfArg - 1))
-        for index, var in enumerate(argv):
-            if index == 0:
-                continue
-            print("{}: {}".format(index, var))
+import sys
+
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(count))
+for i in range(count):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
