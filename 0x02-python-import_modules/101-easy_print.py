89f95d3f8cedfdd66082ba5e935a4bb8f4664af6
olaniyo <opriolani>
2023-01-15 04:04:31 -0800

    advanced

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,6 +1,2 @@
 #!/usr/bin/python3
-
-def remove_char_at(str, n):
-    if n < 0:
-        return (str)
-        return (str[:n] + str[n+1:])
+__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
