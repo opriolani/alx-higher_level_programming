3c12474293d71acacf2c222dc5af6ca299d37258
olaniyo <opriolani>
2023-01-15 03:54:16 -0800

    advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-def magic_calculation(a, b):
-    """Match bytecode provided by Holberton School"""
-    from magic_calculation_102 import add, sub
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
-            return (c)
-        else:
-            return(sub(a, b))
+if __name__ == "__main__":
+    from sys import argv
+    from calculator_1 import add, sub, mul, div
+    if len(argv) != 4:
+        print("Usage:", argv[0], "<a> <operator> <b>")
+        exit(1)
+    a, b = int(argv[1]), int(argv[3])
+    operator = argv[2]
+    math = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in math:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
