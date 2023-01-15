e2d1025dd7143fe35f8a86f19339600a2530f72c
olaniyo <opriolani>
2023-01-15 03:59:49 -0800

    advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-    from calculator_1 import add, sub, mul, div
     if len(argv) != 4:
-        print("Usage:", argv[0], "<a> <operator> <b>")
-        exit(1)
-    a, b = int(argv[1]), int(argv[3])
-    operator = argv[2]
-    math = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in math:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        quit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    from calculator_1 import add, sub, mul, div
+    funcs = [add, sub, mul, div]
+    for i, s in enumerate(ops):
+        if argv[2] == s:
+            print("{} {} {} = {}".format(a, s, b, funcs[i](a, b)))
+            break
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    print("{:d} {} {:d} = {:d}".format(a, operator, b, math[operator](a, b)))
+        quit(1)
