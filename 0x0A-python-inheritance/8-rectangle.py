6eb8643d9875fa94a41add17f417748e96a658f9
olaniyo <opriolani>
2023-02-10 23:34:15 -0800

    task

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -2,9 +2,7 @@
 """
 Contains the class BaseGeometry and subclass Rectangle
 """
-
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 class Rectangle(BaseGeometry):
     """A representation of a rectangle"""
     def __init__(self, width, height):
@@ -12,4 +10,4 @@ class Rectangle(BaseGeometry):
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
-        self.__height = 
+        self.__height = height
