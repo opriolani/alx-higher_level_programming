f858e08ba7ef4f476a3a04220fa8623d3da4c9df
olaniyo <opriolani>
2023-04-13 06:15:57 -0700

    advanced task

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// A script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence
+const dict = require('./101-data').dict;
+
+const totalist = Object.entries(dict);
+const vals = Object.values(dict);
+const valsUniq = [...new Set(vals)];
+const newDict = {};
+for (const j in valsUniq) {
+  const list = [];
+  for (const k in totalist) {
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
+    }
+  }
+  newDict[valsUniq[j]] = list;
+}
+console.log(newDict);
