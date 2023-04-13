9bec839747970525bc32456333c4d301a809c522
olaniyo <opriolani>
2023-04-13 01:52:13 -0700

    advanced task javascript warm-up

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// This script adds a new function incr that increments the integer value
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  this.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
